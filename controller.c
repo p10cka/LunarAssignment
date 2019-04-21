3652a0788ba4f22e2e16262ff871c1e4ddd76f29
Ryanp1998 <42222293+Ryanp1998@users.noreply.github.com>
2019-04-21 11:23:00 +0100

    Update controller.c

--- controller.c
+++ controller.c
@@ -19,7 +19,6 @@
  
 int getaddr(const char *node, const char *service, struct addrinfo **address);
 int makeSocket(void);
-int writeFile(char *text[100]);
 void sendCommand(int fd, struct addrinfo *address);
 void getCondition(int fd, struct addrinfo *address);
 void getUserInput(int fd, struct addrinfo *address);
@@ -134,7 +133,6 @@ void sendCommand(int fd, struct addrinfo *address) {
     char outgoing[buffsize];
  
     snprintf(outgoing, sizeof(outgoing), "command:!\nmain-engine: %i\nrcs-roll: %f", enginePower, rcsRoll);
-    writeFile("command:!\nmain-engine: %i\nrcs-roll: %f", enginePower, rcsRoll);
     sendto(fd, outgoing, strlen(outgoing), 0, address->ai_addr, address->ai_addrlen);
 }
  
@@ -204,14 +202,6 @@ int makeSocket(void) {
     }
  
     return sfd;
-}
-
-int writeFile(char *text[100]) {
-    FILE * fp;
-    fp = fopen("./datalog.txt","w");
-    fprintf (fp, "%s \n", text);
-    fclose(fp);
-    return 0;
 }
  
 int bindSocket(int sfd, const struct sockaddr *addr, socklen_t addrlen) {
